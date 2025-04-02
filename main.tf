fb61963bb8d7c6c870337cfdd01253e02da98ea0
cxhacks_17 <chrisminepro@gmail.com>
2025-04-01 18:44:42 -0600

    Update Main

--- main.tf
+++ main.tf
@@ -61,25 +61,37 @@ module "vpc" {
 #             INSTANCIAS EC2                #
 #############################################
 # Instancia de Windows Server 2019
-resource "aws_launch_configuration" "windows_launch_config" {
-    name          = "windows_launch_config"
-    image_id           = var.windows_ami_id # Reemplazar con la AMI de Windows Server 2019
+resource "aws_instance" "windows_instance" {
+    ami           = var.windows_ami_id # Reemplazar con la AMI de Windows Server 2019
     instance_type = "t2.micro"
+    subnet_id     = aws.subnet.public_subnet.id
+    associate_public_ip_address = true
     security_groups = [ aws_security_group.windows_rdp.id ]
 
     # Usar una configuración de RDP
     user_data = <<-EOF
         <powershell>
-        Enable-NetFirewallRule -DisplayName 'Remote Desktop - User Mode (TCP-In)'
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
+
+        Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 0
+
+        Restart-Service TermService -Force
         </powershell>
     EOF
+
+    tags = {
+        Name = "WindowsInstance"
+    }
 }
 
 # Instancia de Linux Ubuntu
-resource "aws_launch_configuration" "linux_launch_config" {
-    name          = "linux_launch_config"
-    image_id           = var.linux_ami_id # Reemplazar con la AMI de Ubuntu
+resource "aws_instance" "linux_instance" {
+    ami           = var.linux_ami_id # Reemplazar con la AMI de Ubuntu
     instance_type = "t2.micro"
+    subnet_id = aws.subnet.public_subnet.id
+    associate_public_ip_address = true
     security_groups = [ aws_security_group.linux_ssh.id ]
 
     # Usar una configuración de SSH
@@ -90,124 +102,47 @@ resource "aws_launch_configuration" "linux_launch_config" {
         sudo systemctl enable ssh
         sudo systemctl start ssh
     EOF
+    tags = {
+        Name = "LinuxInstance"
+    }
 }
 
 #############################################
 #              Auto Scaling                 #
 #############################################
 # Grupo de Autoscaling para Windows Server 2019
-resource "aws_autoscaling_group" "windows_asg" {
-    desired_capacity     = 2
-    max_size             = 2
-    min_size             = 1
-    vpc_zone_identifier  = module.vpc.private_subnets
-    launch_configuration = aws_launch_configuration.windows_launch_config.id
-}
+# resource "aws_autoscaling_group" "windows_asg" {
+#     desired_capacity     = 2
+#     max_size             = 2
+#     min_size             = 1
+#     vpc_zone_identifier  = module.vpc.private_subnets
+#     launch_configuration = aws_launch_configuration.windows_launch_config.id
+# }
 
 # Grupo de Autoscaling para Linux Ubuntu
-resource "aws_autoscaling_group" "linux_asg" {
-    desired_capacity     = 2
-    max_size             = 2
-    min_size             = 1
-    vpc_zone_identifier  = module.vpc.private_subnets
-    launch_configuration = aws_launch_configuration.linux_launch_config.id
-}
+# resource "aws_autoscaling_group" "linux_asg" {
+#     desired_capacity     = 2
+#     max_size             = 2
+#     min_size             = 1
+#     vpc_zone_identifier  = module.vpc.private_subnets
+#     launch_configuration = aws_launch_configuration.linux_launch_config.id
+# }
 
 #############################################
 #              ELASTIC IPs                  #
 #############################################
 resource "aws_eip" "windows_eip" {}
 
-data "aws_instances" "windows_instances" {
-    filter {
-        name = "tag:aws:autoscaling:groupName"
-        values = [aws_autoscaling_group.windows_asg.name]
-    }
-}
-
 resource "aws_eip_association" "windows_eip_association" {
-    instance_id = tolist(data.aws_instances.windows_instances.ids)[0]
+    instance_id = aws_instance.windows_instance.id
     allocation_id = aws_eip.windows_eip.id
 }
 
 resource "aws_eip" "linux_eip" {}
 
-data "aws_instances" "linux_instances" {
-    filter {
-        name = "tag:aws:autoscaling:groupName"
-        values = [aws_autoscaling_group.linux_asg.name]
-    }
-
-}
-
 resource "aws_eip_association" "linux_eip_association" {
-    instance_id = tolist(data.aws_instances.linux_instances.ids)[0]
+    instance_id = aws_instance.linux_instance.id
     allocation_id = aws_eip.linux_eip.id
-}
-
-#############################################
-#                  EKS                      #
-#############################################
-# Se despliega un clúster EKS en la VPC creada.
-module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "20.8.5"
-
-    cluster_name    = local.cluster_name
-    cluster_version = "1.29"
-
-    # Configuración de acceso público al endpoint del clúster.
-    cluster_endpoint_public_access           = true
-    enable_cluster_creator_admin_permissions = true
-
-    # Se agregan complementos del clúster, como el controlador CSI de EBS.
-    cluster_addons = {
-        aws-ebs-csi-driver = {
-            service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-        }
-    }
-
-    # Se asocia el clúster a la VPC y a sus subredes privadas.
-    vpc_id     = module.vpc.vpc_id
-    subnet_ids = module.vpc.private_subnets
-
-    # Definición del grupo de nodos gestionados por EKS. 
-    # Se habilita el auto scaling y se especifica el tipo de instancia.
-    eks_managed_node_groups = {
-        # Se define el grupo de nodos 1 para Linux.
-        linux_nodes = {
-            name          = "linux_node_group"
-            instance_type = "t2.micro"
-            min_size      = 1
-            max_size      = 2
-            desired_size  = 2
-            node_role_arn = module.eks.cluster_iam_role_arn
-            subnet_ids    = module.vpc.private_subnets  # Subredes privadas para los nodos
-
-            asg_name = aws_autoscaling_group.linux_asg.name  # Nombre del grupo de autoscaling para Linux
-
-            labels = {
-                "node-type" = "linux"
-            }
-        }
-
-        # Se define el grupo de nodos 2 para Windows.
-        windows_nodes = {
-            name          = "windows_node_group"
-            instance_type = "t2.micro"
-            min_size      = 1
-            max_size      = 2
-            desired_size  = 2
-            node_role_arn = module.eks.cluster_iam_role_arn
-            subnet_ids    = module.vpc.private_subnets  # Subredes privadas para los nodos
-
-            asg_name = aws_autoscaling_group.windows_asg.name  # Nombre del grupo de autoscaling para Windows
-
-            labels = {
-                "node-type" = "windows"
-            }
-        }
-    }
 }
 
 #############################################
@@ -254,6 +189,101 @@ resource "aws_security_group" "windows_rdp" {
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+#############################################
+#                  EKS                      #
+#############################################
+# Se despliega un clúster EKS en la VPC creada.
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "20.8.5"
+
+    cluster_name    = local.cluster_name
+    cluster_version = "1.29"
+
+    # Configuración de acceso público al endpoint del clúster.
+    cluster_endpoint_public_access           = true
+    enable_cluster_creator_admin_permissions = true
+
+    # Se agregan complementos del clúster, como el controlador CSI de EBS.
+    cluster_addons = {
+        aws-ebs-csi-driver = {
+            service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+        }
+    }
+
+    # Se asocia el clúster a la VPC y a sus subredes privadas.
+    vpc_id     = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
+
+    # Definición del grupo de nodos gestionados por EKS. 
+    # Se habilita el auto scaling y se especifica el tipo de instancia.
+    eks_managed_node_groups = {
+        # Se define el grupo de nodos 1 para Linux.
+        linux_nodes = {
+            name          = "linux_node_group"
+            instance_type = "t2.micro"
+            min_size      = 1
+            max_size      = 2
+            desired_size  = 2
+            node_role_arn = aws_iam_role.eks_node_role.arn  # Rol de IAM para los nodos
+            subnet_ids    = module.vpc.private_subnets  # Subredes privadas para los nodos
+
+            labels = {
+                "node-type" = "linux"
+            }
+        }
+
+        # Se define el grupo de nodos 2 para Windows.
+        windows_nodes = {
+            name          = "windows_node_group"
+            instance_type = "t2.micro"
+            min_size      = 1
+            max_size      = 2
+            desired_size  = 2
+            node_role_arn = aws_iam_role.eks_node_role.arn  # Rol de IAM para los nodos
+            subnet_ids    = module.vpc.private_subnets  # Subredes privadas para los nodos
+            labels = {
+                "node-type" = "windows"
+            }
+        }
+    }
+}
+
+#############################################
+#                  Node                     #
+#############################################
+# Se crea el rol de IAM para los nodos EKS, permitiendo el acceso a la VPC y a los recursos de AWS.
+resource "aws_iam_role" "eks_node_role" {
+    name               = "${local.cluster_name}-node-role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect = "Allow"
+            Action = "sts:AssumeRole"
+            Principal = {
+            Service = "ec2.amazonaws.com"
+            }
+        }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+    role       = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 #############################################
